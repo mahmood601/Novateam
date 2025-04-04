31b18956138eb18766065ef1e32c754fe7a28529
Mahmood <mahmoud21501@gmail.com>
2025-04-04 21:31:35 +0300

    add vitte pwa patches

--- src/components/UI.tsx
+++ src/components/UI.tsx
@@ -2,9 +2,10 @@ import { For } from "solid-js";
 import Box from "./Box";
 import "../index.css";
 import subjects from "./subjects";
+import { inject } from "@vercel/analytics";
 
 export default function UI() {
-  inject
+  inject()
   return (
     <div class="bg-main-light dark:bg-main-dark pt-10 shrink-1 overflow-x-hidden">
       <div class="flex h-full w-full justify-center overflow-hidden">
