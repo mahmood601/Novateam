4522edba166661482139381ca7681d6e99091fae
Mahmood <mahmoud21501@gmail.com>
2025-04-11 08:04:49 +0300

    add fifth year support

--- src/components/years.ts
+++ src/components/years.ts
@@ -1,38 +1,53 @@
- const years = {
+const years = {
   // This file should be updated every year and season
   second: {
     name: "السنة الثانية",
-    subjects:[
-    "anatomy-2",
-    "physiology",
-    "clinical-chemistry",
-    "proper-histology",
-    "embryology",
-    "md-4",
-    "communication-skills",
-  ]},
-  third:{
+    subjects: [
+      "anatomy-2",
+      "physiology",
+      "clinical-chemistry",
+      "proper-histology",
+      "embryology",
+      "md-4",
+      "communication-skills",
+    ],
+  },
+  third: {
     name: "السنة الثالثة",
-    subjects:[
-    "anatomy-2",
-    "physiology",
-    "clinical-chemistry",
-    "proper-histology",
-    "embryology",
-    "md-4",
-    "communication-skills",
-  ]},
+    subjects: [
+      "anatomy-2",
+      "physiology",
+      "clinical-chemistry",
+      "proper-histology",
+      "embryology",
+      "md-4",
+      "communication-skills",
+    ],
+  },
   fourth: {
     name: "السنة الرابعة",
-    subjects:[
-    "anatomy-2",
-    "physiology",
-    "clinical-chemistry",
-    "proper-histology",
-    "embryology",
-    "md-4",
-    "communication-skills",
-  ]},
+    subjects: [
+      "anatomy-2",
+      "physiology",
+      "clinical-chemistry",
+      "proper-histology",
+      "embryology",
+      "md-4",
+      "communication-skills",
+    ],
+  },
+  fifth: {
+    name: "السنة الخامسة",
+    subjects: [
+      "anatomy-2",
+      "physiology",
+      "clinical-chemistry",
+      "proper-histology",
+      "embryology",
+      "md-4",
+      "communication-skills",
+    ],
+  },
 };
 
 export default years;
