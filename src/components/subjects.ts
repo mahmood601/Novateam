583cf7cd75ab11f7a76dfd92f17be16905bc49fb
Mahmood <mahmoud21501@gmail.com>
2025-07-27 18:15:58 +0300

    fix: number of seasons

--- src/components/subjects.ts
+++ src/components/subjects.ts
@@ -103,6 +103,7 @@ const subjects = {
       { id: 2, name: "second" },
       { id: 3, name: "third" },
       { id: 4, name: "fourth" },
+      { id: 5, name: "fifth" },
       { id: 6, name: "fifth" },
       { id: 7, name: "fifth" },
       { id: 8, name: "fifth" },
